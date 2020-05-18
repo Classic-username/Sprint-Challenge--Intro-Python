764cb029ff4bbc76ce2b5e8ee1ecbf32a819a8d6
Joshua Draper <josh.drapergame@gmail.com>
2020-05-17 21:18:04 -0600

    Cityreader done, and coprehensions passes.

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -62,7 +62,7 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = [(human.name.upper(), human.age + 5) for human in humans]
+g = [Human(human.name.upper(), human.age + 5) for human in humans.copy()]
 # g = [human for human in humans]
 print(g)
 
